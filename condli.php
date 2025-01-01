da11f3d9d26c098c60d7f53274fcbf8bd77c53e5
DLII34 <mardiyanandika@gmail.com>
2025-01-01 09:01:59 +0700

    Update condli.php

--- condli.php
+++ condli.php
@@ -2,7 +2,7 @@
 $servername = "localhost";  // Ganti dengan server database Anda jika perlu
 $username = "root";         // Ganti dengan username database Anda
 $password = "";             // Ganti dengan password database Anda
-$dbname = "dli_billing";    // Ganti dengan nama database Anda
+$dbname = "db_name";    // Ganti dengan nama database Anda
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
